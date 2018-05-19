2d3d9bf08ba1862877f5871cd0c42ed8275f5f08
Tyler <rothtyler24@gmail.com>
2018-05-18 17:56:56 -0700

    I reviewed dictionaries and learned about infix operators.

--- Playground.playground/Pages/Untitled Page 4.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Untitled Page 4.xcplaygroundpage/Contents.swift
@@ -36,4 +36,33 @@ else {
     
 }
 
+
+infix operator ??
+
+func ?? <T>(optional:T?, defaultValue: () -> T) -> T {
+    if let unwrappedOptional = optional {
+        return unwrappedOptional
+    }
+    else {
+        return defaultValue()
+    }
+}
+
+var masses = [
+    "Sun"              : 1.989E30,
+    "Earth"            : 5.972E24,
+    "Moon"             : 7.348E22,
+    "Betelgeuse"       : 1.531E31,
+    "Proxima Centauri" : 2.446E29
+]
+
+//if let unwrappedMoonMass = masses["Moon"] {
+//    let moonMass = unwrappedMoonMass
+//}
+//else {
+//    let moonMass = 0
+//}
+
+let moonMass: Double = masses["Moon"] ?? 0.0
+
 //: [Next](@next)
